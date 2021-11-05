cd4690f04c42f7eb6cf3bbd6c166c70f9a4eff55
dunglevan-design <dunglevan2001@gmail.com>
2021-11-05 15:09:30 +0000

    push notifications

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, FacebookAuthProvider, } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-
+import {getMessaging, getToken } from "firebase/messaging"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +25,28 @@ export const db = getFirestore()
 export const Googleprovider = new GoogleAuthProvider();
 export const Facebookprovider = new FacebookAuthProvider();
 export const auth = getAuth();
+
+// Initialize messaging to request notifications.
+
+export const messaging = getMessaging(app);
+
+// Add the public key generated from the console here.
+/**Only works on build version. For testing, generate token manually */
+// getToken(messaging, { vapidKey: 'BLFRbbKh7i-skCjTBESy0m4xdv_644zN92do2g98YC0BUt-SBgYcj0hb2NFE584YDKAZChVS01AfgMsEpT6Xgs8' }).then((currentToken) => {
+//   if (currentToken) {
+//     console.log(currentToken, "watching");
+//     // ...
+//   } else {
+//     // Show permission request UI
+//     console.log('No registration token available. Request permission to generate one.');
+//     // ...
+//   }
+// }).catch((err) => {
+//   console.log('An error occurred while retrieving token. ', err);
+//   // ...
+// });
+
+export const token = "dXNiY7Fql_l8CBLLeXVvXf:APA91bFsPuEJC9xZDZfm5gYO5YxEWhCkhax3V2iuUh6jIeyeekYU70WMuLePE9R1baqS1F8pETb-8NkBNtOdOb9Hf-uTmkdBWv72T_tMXIwv-w61SRF7NxfNah4ECUqC3XaZ7XXr2SBh";
+
+
+// getToken(messaging,{vapidKey: 'BHF4Im3xkuXDg_JlL-f4SMKRKFULrsBRwZPWQ-yxMVOId0XX8z7DMPVkrzX1cT5d-9sYyqmuQNAMZqsxLia_qEA'}).then((currentToken) => console.log(currentToken));
